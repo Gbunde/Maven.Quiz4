5fe90d301e9e4eaeece00e5a9ec6407694838157
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-09 19:28:14 -0500

    some tests passed

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,22 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class WordCounter {
+    private String[] strArr;
+    private LinkedHashMap<String, Integer> wordMap;
     public WordCounter(String... strings) {
+        this.strArr = strings;
+        this.wordMap = new LinkedHashMap<>();
+
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        for (String str : this.strArr){
+            this.wordMap.merge(str, 1, Integer::sum);
+        }
+        return this.wordMap;
     }
-}
+    }
+
