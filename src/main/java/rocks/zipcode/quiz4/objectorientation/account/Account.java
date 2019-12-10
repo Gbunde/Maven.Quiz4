99050ad3a34b3d0cfdf0f9144ef001c06d1a9cca
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-09 19:46:44 -0500

    some tests passed

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Account.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Account.java
@@ -4,10 +4,22 @@ package rocks.zipcode.quiz4.objectorientation.account;
  * @author leon on 30/12/2018.
  */
 public class Account extends BankAccount {
+    Long id;
+
+    public Account(){
+       this.getId();
+
+    }
+    public Account(Long id){
+        this.id = id;
+    }
+
+
     public Long getId() {
-        return null;
+        return this.id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 }
